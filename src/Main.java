dd1a282ade2bf02efc2184c06f4c0129251fa16d
Vasyl Kuzurman <fasd123435@gmail.com>
2021-08-17 09:42:15 +0200

    src/Main.java Added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int recharge = 1200;
+        int recharge = 1100;
         int totalBonus;
         if (recharge > 1000) {
             totalBonus = recharge / account;
         } else {
-            totalBonus = account + recharge;
+            totalBonus = 0;
         }
         System.out.println("Бонус за пополнение:" + totalBonus);
 
